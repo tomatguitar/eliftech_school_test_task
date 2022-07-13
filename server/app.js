5e30593f14475e859841ca16ddae9b523d57a164
Dmytro Zhyltsov <tomatguitar@gmail.com>
2022-07-13 22:53:36 +0300

    added headers

--- server/app.js
+++ server/app.js
@@ -34,6 +34,12 @@ app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 
 // ROUTES
 app.use('/api/shops', shopsRouter);
